f6a4dc4fdfb99be81aa8fef0a792a3642a92f511
Shiplu Hawlader <shiplu.cse.du@gmail.com>
2020-08-20 22:00:25 -0500

    Implemented features like teacher broadcast, see queue, choose from queue, student get and submit. Also, initial version of teacher login

--- controllers.py
+++ controllers.py
@@ -28,12 +28,167 @@ Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app w
 from py4web import action, request, abort, redirect, URL
 from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated
-
+import datetime
+import json
+from .models import add_or_update_score
 
 @unauthenticated()
 @action('index')
 @action.uses(auth, 'index.html')
 def index():
     user = auth.get_user()
-    message = T("Hello {first_name}".format(**user))
+    # message = T("Hello {first_name}".format(**user))
+    message = 'Hello World'
     return dict(message=message)
+
+@unauthenticated
+@action('teacher_broadcasts', method='POST')
+# @action.uses('index.html')
+def teacher_broadcasts():
+    content = request.POST['content']
+    answer = request.POST['answer']
+    merit = int(request.POST['merit'])
+    effort = int(request.POST['effort'])
+    attempts = int(request.POST['attempts'])
+    topic_id = 0 #int(request.POST['topic_id'])
+    tag = request.POST['tag']
+    filename = request.POST['filename']
+    exact_answer = 1 if request.POST['exact_answer']=='True' else 0
+    # print(request.query)
+    # return dict(message='')
+    rows = db(db.tag.topic_description == tag).select()
+    if len(rows)==0:
+        tag_id = db.tag.insert(topic_description=tag)
+    else:
+        for row in rows:
+            tag_id = row.id
+            break
+    pid = db.problem.insert(teacher_id=1, problem_description=content, answer=answer, filename=filename, merit=merit, effort=effort, attempts=attempts, topic_id=topic_id, tag=tag_id, problem_uploaded_at=datetime.datetime.now(), exact_answer=exact_answer, active=1)
+    # print(content)
+    # print(answer)
+    return 'Content copied to white boards.'
+
+
+@unauthenticated
+@action('ask', method='POST')
+# @action.uses('index.html')
+def ask():
+    path = request.environ['PATH_INFO'].split('/')[1]
+    url = '%s://%s/%s/' % (request.environ['wsgi.url_scheme'], request.environ['HTTP_HOST'], path)
+    return url
+    
+
+@unauthenticated
+@action('student_gets', method='POST')
+def student_gets():
+    boards = []
+    for row in db(db.problem.active==1).select():
+        boards.append({'Content': row.problem_description, 'Filename': row.filename, 'Type': 'Question'})
+    return json.dumps(boards)
+
+@unauthenticated
+@action('student_shares', method='POST')
+def student_shares():
+    content = request.POST['content']
+    filename = request.POST['filename']
+    answer = request.POST['answer']
+    priority = int(request.POST['priority'])
+    uid = request.POST['uid']
+    prob = None
+    verdict = ""
+    for row in db(db.problem.filename==filename).select():
+        prob = row
+        break
+    scoring_msg = "Your submission will be looked at soon."
+    msg = ""
+    complete = False
+    if prob is not None:
+        if prob.active != 1:
+            msg = "Problem is no longer active. But the teacher will look at your submission."
+        else:
+            attempts = db.attempt(problem_id=prob.id, student_id=uid)
+            if attempts is None:
+                db.attempt.insert(problem_id=prob.id, student_id=uid, remaining_attempt=prob.attempts)
+                attempts = db.attempt(problem_id=prob.id, student_id=uid)
+
+            if attempts is not None and attempts.remaining_attempt==0:
+                return "This is not submitted because either you have reached the submission limit or your solution was previously graded correctly."
+            db(db.attempt.id==attempts.id).update(remaining_attempt=attempts.remaining_attempt-1)
+            if priority < 2 :
+                if attempts.remaining_attempt<=3:
+                    msg = " You have " + str(attempts.remaining_attempt-1)+" attempt(s) left."
+            
+
+            correct_answer = prob.answer
+            if answer != "":
+                if correct_answer == answer:
+                    scoring_msg = add_or_update_score("correct", prob.id, uid, 1, -1)
+                    print(attempts)
+                    db(db.attempt.id == attempts.id).update(remaining_attempt=0)
+                    complete = True
+                    verdict = "correct"
+                elif prob.exact_answer == 1:
+                    scoring_msg = add_or_update_score("incorrect", prob.id, uid, 1, -1)
+                    complete = True
+                    verdict = "incorrect"
+                else:
+                    scoring_msg = "Answer appears to be incorrect. It will be looked at. "
+            
+            if complete == True:
+                db.submission.insert(problem_id=prob.id, student_id=uid, student_code=content, submission_category=priority, code_submitted_at=datetime.datetime.now(), completed=datetime.datetime.now(), looked_at = datetime.datetime.now(), verdict=verdict)
+            else:
+                db.submission.insert(problem_id=prob.id, student_id=uid, student_code=content, submission_category=priority, code_submitted_at=datetime.datetime.now())
+            db.commit()
+        if verdict == "correct":
+            return scoring_msg
+        return scoring_msg+msg
+
+
+@unauthenticated
+@action('teacher_gets_queue', method='POST')
+def teacher_gets_queue():
+    submissions = []
+    for sub in db((db.submission.completed == None)&(db.submission.looked_at==None)).select(orderby=db.submission.code_submitted_at):
+        submissions.append({'Sid': sub.id, 'Uid': sub.student_id, 'Pid': sub.problem_id, 'Content': sub.student_code, 'Filename': db.problem[sub.problem_id].filename, 'Priority': sub.submission_category, 'Name': db.student[sub.student_id].name})
+    return json.dumps(submissions)
+
+@unauthenticated
+@action('teacher_gets', method='POST')
+def teacher_gets():
+    index = int(request.POST['index'])
+    priority = int(request.POST['priority'])
+    selected = {}
+    workingsubs = db((db.submission.completed == None)&(db.submission.looked_at==None)).select(orderby=db.submission.code_submitted_at)
+    if index >= 0:
+        sub = workingsubs[index]
+        selected = {'Sid': sub.id, 'Uid': sub.student_id, 'Pid': sub.problem_id, 'Content': sub.student_code, 'Filename': db.problem[sub.problem_id].filename, 'Priority': sub.submission_category, 'Name': db.student[sub.student_id].name}
+        db.submission(sub.id).update(looked_at=datetime.datetime.now())
+    elif priority > 0:
+
+        for sub in workingsubs:
+            if sub.priority == priority:
+                selected = {'Sid': sub.id, 'Uid': sub.student_id, 'Pid': sub.problem_id, 'Content': sub.student_code, 'Filename': db.problem[sub.problem_id].filename, 'Priority': sub.submission_category, 'Name': db.student[sub.student_id].name}
+                db.submission(sub.id).update(looked_at=datetime.datetime.now())
+                break
+    else:
+        first_sub_w_priority = [-1, -1, -1]
+        for i in range(len(workingsubs)):
+            p = workingsubs[i].priority
+            if first_sub_w_priority[p] == -1:
+                first_sub_w_priority[p] = i
+        for i in range(len(first_sub_w_priority)-1, -1, 0):
+            if first_sub_w_priority[i] != -1:
+                j = first_sub_w_priority[i]
+                sub = workingsubs[j]
+                selected = {'Sid': sub.id, 'Uid': sub.student_id, 'Pid': sub.problem_id, 'Content': sub.student_code, 'Filename': db.problem[sub.problem_id].filename, 'Priority': sub.submission_category, 'Name': db.student[sub.student_id].name}
+                db.submission(sub.id).update(looked_at=datetime.datetime.now())
+                break
+    return selected
+
+
+@unauthenticated
+@action('teacher_puts_back', method='POST')
+def teacher_puts_back():
+    sid = int(request.POST['sid'])
+    db.submission(sid).update(looked_at=None)
+    return "Submission has been put back into the queue."
--- models.py
+++ models.py
@@ -5,6 +5,7 @@ This file defines the database models
 from .common import db, Field
 from pydal.validators import *
 
+import datetime
 ### Define your table below
 #
 # db.define_table('thing', Field('name'))
@@ -13,3 +14,56 @@ from pydal.validators import *
 #
 # db.commit()
 #
+def create_tables():
+    db.define_table('student', Field('name'), Field('password'))
+    db.define_table('teacher', Field('name'), Field('password'))
+    db.define_table('course', Field('course_id', notnull=True), Field('course_name'), Field('semester'), Field('Year', type='integer'), Field('teacher_id', type='reference teacher'))
+    db.define_table('attendance', Field('student_id', type='reference student', notnull=True), Field('attendance_at', type='datetime'))
+    db.define_table('tag', Field('topic_description', unique=True))
+    db.define_table('problem', Field('teacher_id', type='reference teacher'), Field('problem_description', type='text'), Field('answer', type='text'),\
+     Field('filename'), Field('merit', type='integer'), Field('effort', type='integer'), Field('attempts', type='integer'), Field('topic_id', type='integer'),\
+     Field('tag', type='integer'), Field('problem_uploaded_at', type='datetime'), Field('exact_answer', type='integer'), Field('active', type='integer'), redefine=True) 
+    db.define_table('submission', Field('problem_id', type='reference problem'), Field('student_id', type='reference student'), Field('student_code', type='text'), Field('submission_category',\
+         type='integer'), Field('code_submitted_at', type='datetime'), Field('completed', type='datetime'), Field('verdict'), Field('looked_at', type='datetime'), redefine=True)
+    db.define_table('score', Field('problem_id', type='reference problem'), Field('student_id', type='reference student'), Field('teacher_id', type='reference teacher'), Field('score', type='integer'),\
+         Field('graded_submission_number', type='integer'), Field('score_given_at', type='datetime'))
+    db.define_table('feedback', Field('teacher_id', type='reference teacher'), Field('feedback', type='text'), Field('feedback_given_at', type='datetime'), Field('submission_id', type='reference submission'))
+    
+    db.define_table('attempt', Field('problem_id', type='reference problem'), Field('student_id', tyep='reference student'), Field('remaining_attempt', type='integer'))
+    db.commit()
+
+
+def add_or_update_score(decision, problem_id, student_id, teacher_id, partial_credits):
+     mesg = ""
+     
+     score_id, current_points, current_attempts, current_tid = 0, 0, 0, 0
+
+     row = db.score(problem_id=problem_id, student_id=student_id)
+     if row is not None:
+          score_id, current_points, current_attempts, current_id = row.id, row.score, row.graded_submission_number, row.teacher_id
+
+     merit, effort = 0, 0
+     row = db.problem(problem_id)
+     if row is not None:
+          merit = row.merit
+          effort = row.effort
+     points, teacher = 0, teacher_id
+     if decision == "correct":
+          points = merit
+          mesg = "Answer is correct."
+     else:
+          if partial_credits < merit:
+               points = partial_credits
+          else:
+               points = effort
+          
+          if points < current_points:
+               points = current_points
+          
+          mesg = "Answer is incorrect."
+     if score_id == 0:
+          db.score.insert(problem_id=problem_id, student_id=student_id, teacher_id=teacher_id, score=points, graded_submission_number=current_attempts+1, score_given_at=datetime.datetime.now())
+     else:
+          db(db.score.id==score_id).update(teacher_id=teacher_id, score=points, graded_submission_number=current_attempts+1)
+     
+     return mesg
